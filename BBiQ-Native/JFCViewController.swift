524180763ab1c14a5bbe2f394a8ebb34e29066cc
Jeffrey Fulton <jeffrey.fulton@me.com>
2014-12-06 03:25:17 -0600

    Animations with background blur... awesome

--- BBiQ-Native/JFCViewController.swift
+++ BBiQ-Native/JFCViewController.swift
@@ -10,10 +10,52 @@ import UIKit
 
 class JFCViewController: UIViewController {
 
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var backgroundView: UIImageView!
+    @IBOutlet weak var backgroundBlurView: UIImageView!
+    var plusButtonImageView: UIImageView!;
+    var animationIsToggled = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        addPlusBarButtonItem()
+    }
+    
+    func addPlusBarButtonItem() {
+        var image = UIImage(named: "plus")
+        image = image!.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate)
+        plusButtonImageView = UIImageView(image: image)
+        plusButtonImageView.autoresizingMask = UIViewAutoresizing.None
+        plusButtonImageView.contentMode = UIViewContentMode.Center
+        
+        var plusButton = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+        plusButton.frame = CGRectMake(0, 0, 40, 40)
+        plusButton.addSubview(plusButtonImageView)
+        plusButton.addTarget(self, action: "toggleAnimation", forControlEvents: UIControlEvents.TouchUpInside)
+        plusButtonImageView.center = plusButton.center
+        var barItem = UIBarButtonItem(customView: plusButton)
+        self.navigationItem.rightBarButtonItem = barItem
+    }
+    
+    func toggleAnimation() {
+        UIView.animateWithDuration(0.5, animations: {
+            if self.animationIsToggled {
+                self.blurView.frame.origin.x = 0
+                self.backgroundView.frame.origin.x = 0
+                self.backgroundBlurView.frame.origin.x = 0
+                self.backgroundBlurView.alpha = 0.0
+                self.plusButtonImageView.transform = CGAffineTransformMakeRotation(0.0)
+            } else {
+                self.blurView.frame.origin.x -= self.blurView.frame.size.width
+                self.backgroundView.frame.origin.x -= 100
+                self.backgroundBlurView.frame.origin.x -= 100
+                self.backgroundBlurView.alpha = 1.0
+                self.plusButtonImageView.transform = CGAffineTransformMakeRotation(CGFloat(45.0 * M_PI / 180.0))
+            }
+            self.animationIsToggled = !self.animationIsToggled
+        })
     }
 
     override func didReceiveMemoryWarning() {
